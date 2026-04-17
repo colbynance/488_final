63bdfb1cd242d1a84d0264e6a386e96d0f0a940b
mdrobot7 <55768773+mdrobot7@users.noreply.github.com>
2026-04-16 21:58:43 -0500

    full verilog design done????
    Time for TB and AXI wrapper

--- SA2_final_project/SA2_final_project.srcs/sources_1/new/analyzer_channel.v
+++ SA2_final_project/SA2_final_project.srcs/sources_1/new/analyzer_channel.v
@@ -1,22 +1,22 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 04/16/2026 06:16:47 PM
-// Design Name: 
+// Design Name:
 // Module Name: analyzer_channel
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 
@@ -24,8 +24,93 @@ module analyzer_channel (
     input wire clk_i,
     input wire rst_i,
 
+    input wire enable_i,
+
     input wire sig_i,
-    // TODO
+
+    input wire [31:0] downsample_spacing_i,
+
+    input wire [31:0] trig_type_i,
+    input wire [31:0] trig_data_i,
+    input wire [31:0] trig_mask_i,
+
+    output wire trig_trigd_o,
+    output wire sample_done_o,
+
+    input wire         buf_we_i,
+    input wire  [9:0]  buf_addr_i,
+    input wire  [31:0] buf_di_i,
+    output wire [31:0] buf_do_o,
 );
+
+wire downsamp_sig;
+
+wire trig_sig;
+wire trig_trig;
+
+wire        streamer_we;
+wire [9:0]  streamer_addr;
+wire [31:0] streamer_di;
+
+wire        buffer_we   = enable_i ? streamer_we   : buf_we_i;
+wire [9:0]  buffer_addr = enable_i ? streamer_addr : buf_addr_i;
+wire [31:0] buffer_di   = enable_i ? streamer_di   : buf_di_i;
+
+downsample downsamp (
+    .clk_i(clk_i),
+    .rst_i(rst_i),
+
+    .sig_i(sig_i),
+    .sig_o(downsamp_sig),
+
+    .sample_spacing_i(downsample_spacing_i)
+);
+
+trigger trig (
+    .clk_i(clk_i),
+    .rst_i(rst_i),
+    .sync_rst_i(!enable_i),
+
+    .sig_i(downsamp_sig),
+
+    .trig_type_i(trig_type_i),
+    .trig_data_i(trig_data_i),
+    .trig_mask_i(trig_mask_i),
+
+    .sig_o(trig_sig),
+    .trig_o(trig_trig)
+);
+
+streamer stream (
+    .clk_i(clk_i),
+    .rst_i(rst_i),
+    .sync_rst_i(!enable_i),
+
+    .sig_i(trig_sig),
+    .trig_i(trig_trig),
+
+    .buffer_we_o(streamer_we),
+    .buffer_addr_o(streamer_addr),
+    .buffer_di_o(streamer_di),
+
+    .sample_done_o(sample_done_o)
+);
+
+buffer buff (
+    .clk(clk_i),
+    .we(buffer_we),
+    .en(1'b1),
+    .addr(buffer_addr),
+    .di(buffer_di),
+    .do(buf_do_o),
+);
+
+always @(posedge clk_i, posedge rst_i) begin
+    if (rst_i) begin
+    end
+    else if (clk_i) begin
+
+    end
+end
 
 endmodule
